771bcb3ad4576508e888bb670a80b2e63cb7130d
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-05 15:23:11 +0700

    perf(ui): move data to package.json and add more margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,13 @@ import pjson from "../../package.json";
 
 const Footer = () => {
   return (
-    <span tw="mb-3 mr-3 text-base text-right font-mono text-white">
-      Create by Kamontat Chantrachirathumrong{" "}
-      <a tw="text-indigo-300 hover:text-indigo-900" href="https://kamontat.net">
-        kamontat.net
-      </a>{" "}
-      (v{pjson.version})
+    <span tw="mb-4 mr-4 text-base text-right font-mono text-white">
+      Create by {pjson.author.name}{" "}
+      <a tw="text-indigo-300 hover:text-indigo-900" href={pjson.author.url}>
+        {pjson.author.url}
+      </a>
+      <br />
+      {pjson.name} v{pjson.version}
     </span>
   );
 };
