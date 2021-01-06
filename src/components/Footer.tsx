0f8652e3a6004e9717fa1087edc916f8d6058709
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-06 19:17:55 +0700

    fix(ui): open new tab when click on the link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
   return (
     <span tw="mb-4 mr-4 text-base text-right font-mono text-white">
       Create by {author.name}{" "}
-      <a tw="text-indigo-300 hover:text-indigo-900" href={author.url}>
+      <a tw="text-indigo-300 hover:text-indigo-900" href={author.url} target="_blank" rel="noreferrer">
         {author.url}
       </a>
       <br />
-      <a tw="text-indigo-300 hover:text-indigo-900" href={repository.url}>
+      <a tw="text-indigo-300 hover:text-indigo-900" href={repository.url} target="_blank" rel="noreferrer">
         {name}
       </a>{" "}
       v{version}
