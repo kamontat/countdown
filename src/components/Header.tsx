aa9cc30da47127308dc0284d23a391e852c22253
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-06 19:12:43 +0700

    feat(ui): add date name to website header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import "twin.macro";
+import tw from "twin.macro";
 import { Property } from "../models/Property";
 
 interface HeaderProperty extends Property {
+  name: string;
   timestamp: number;
 }
 
-const Header = ({ timestamp }: HeaderProperty) => {
-  return <span tw="mt-4 mr-4 text-base text-right font-mono text-white">{timestamp}</span>;
+const Container = tw.div`
+  flex justify-between
+`;
+
+const Text = tw.span`
+  m-4 text-base font-mono text-white
+`;
+
+const Header = ({ name, timestamp }: HeaderProperty) => {
+  return (
+    <Container>
+      <Text>{name}</Text>
+      <Text>{timestamp}</Text>
+    </Container>
+  );
 };
 
 export { Header };
