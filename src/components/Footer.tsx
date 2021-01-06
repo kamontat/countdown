9e18e179ac4f37f0b4561e9921cc92ac9d02da87
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-06 18:44:46 +0700

    fix(perf): reduce loading size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import "twin.macro";
 
-import pjson from "../../package.json";
+import { author, name, version, repository } from "../../package.json";
 
 const Footer = () => {
   return (
     <span tw="mb-4 mr-4 text-base text-right font-mono text-white">
-      Create by {pjson.author.name}{" "}
-      <a tw="text-indigo-300 hover:text-indigo-900" href={pjson.author.url}>
-        {pjson.author.url}
+      Create by {author.name}{" "}
+      <a tw="text-indigo-300 hover:text-indigo-900" href={author.url}>
+        {author.url}
       </a>
       <br />
-      <a tw="text-indigo-300 hover:text-indigo-900" href={pjson.repository.url}>
-        {pjson.name}
+      <a tw="text-indigo-300 hover:text-indigo-900" href={repository.url}>
+        {name}
       </a>{" "}
-      v{pjson.version}
+      v{version}
     </span>
   );
 };
