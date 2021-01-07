abf57a07f994ad242b7b68806f8a89ee8e919a1b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-07 12:44:53 +0700

    perf(core): implement logger with logging object to support debug mode

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import { GlobalStyles } from "twin.macro";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
+import { DataLayer } from "google-tag-manager";
+
 import "./fonts/ttf/JetBrainsMono-Bold.ttf";
 import "./fonts/ttf/JetBrainsMono-Italic.ttf";
 import "./fonts/ttf/JetBrainsMono-Regular.ttf";
@@ -17,6 +19,8 @@ import "./fonts/webfonts/JetBrainsMono-Italic.woff2";
 import "./fonts/webfonts/JetBrainsMono-Regular.woff2";
 
 import "./index.css";
+import { Logger } from "./models/Logger";
+const logger = new Logger("index");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -29,9 +33,36 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onSuccess: (_, isLocalhost) => {
+    logger.debug("Content is cached for offline use.");
+    if (isLocalhost) {
+      logger.warn(
+        "This web app is being served cache-first by a service worker. To learn more, visit https://cra.link/PWA"
+      );
+    }
+  },
+  onUpdate: () =>
+    logger.debug(
+      "New content is available and will be used when all tabs for this page are closed. See https://cra.link/PWA."
+    ),
+  onError: error => logger.error("Error: %o", error),
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+declare const dataLayer: DataLayer;
+reportWebVitals(({ id, name, delta }) => {
+  if (dataLayer) {
+    dataLayer.push({
+      event: "web-vitals",
+      event_category: "Web Vitals",
+      event_action: name,
+      event_value: Math.round(name === "CLS" ? delta * 1000 : delta),
+      event_label: id,
+    });
+  } else {
+    logger.warn("cannot send %s to gtm (%s)", name, id);
+  }
+});
