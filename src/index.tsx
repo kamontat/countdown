65de381ad2d97d076b4a0e2aed8f68e4e5aba9df
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-07 11:22:58 +0700

    feat(ui): add `progressive web app` supported

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import { IndexPage } from "./pages/Index";
 import { GlobalStyles } from "twin.macro";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 import "./fonts/ttf/JetBrainsMono-Bold.ttf";
@@ -24,6 +25,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
